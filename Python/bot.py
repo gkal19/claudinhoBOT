94514ddb31e48acf5408c531c2f728e66675a22f
Gabriel <gabrielsilva1956@gmail.com>
2017-04-06 22:49:24 -0300

    Melhorias no comando soma

--- Python/bot.py
+++ Python/bot.py
@@ -1,3 +1,5 @@
+#! /usr/bin/env Python
+
 import os #Importar os
 import random #Importar random
 import time #Importar time
@@ -10,22 +12,22 @@ from datetime import date #Importar date de datatime
 
 for i in range(1, 7):
     # Vai escolher aleatoriamente entre essas saudações
-    greeting = random.choice(("Ei", "Oi", "Olá", "Papai, é você?", "E aí", "kk eae men"))
+    greeting = random.choice(("Oi", "Olá", "Papai, é você?", "E aí", "kk eae men", "Irineu"))
     # Imprimir alguma saudação aleatória
     print("BOT:", greeting)
     #str(os.system("BOT:" +greeting+ ""))
 
     # O usuário digita
     answer = input("VOCÊ: ")
-    # Verifica se o usuário digitou Irineu ou irineu
-    if answer == "Irineu" or answer == "irineu":
-        # Após verificar, é imprimido "Você não sabe e nem eu..."
-        print("BOT: Você não sabe e nem eu...")
+    # Verifica se o usuário digitou seu pai
+    if "seu pai" in answer:
+        # Após verificar, é imprimido "Você não é meu pai, porém como eu posso te ajudar?"
+        print("BOT: Você não é meu pai, porém como eu posso te ajudar?")
 
     # Verifica qualquer outra resposta da saudação    
     if answer not in greeting:
         # O BOT pergunta ao usuário como ele poderia o ajudar aleatoriamente
-        howICanHelpYou = random.choice(("Você não é meu pai, porém como eu posso te ajudar?", "Como eu posso te ajudar?"))
+        howICanHelpYou = random.choice(("Precisa de ajuda em quê?", "Como eu posso te ajudar?"))
         # Uma pergunta é impressa
         print("BOT:", howICanHelpYou)
         #os.system("BOT:" +howICanHelpYou+ ".")
@@ -40,9 +42,9 @@ for i in range(1, 7):
     
     while 1:
 
-        print("\nBOT: Comandos disponíveis:\nHorário\nSexo\nPiada", "\n")
+        print("\nBOT: Comandos disponíveis:\nHorário\nSexo\nPiada\nSomar", "\n")
         
-        commands = ("horario", "sexo", "piada")
+        commands = ("horario", "sexo", "piada","somar")
         # O usuário digita qual comando quer usar
         chooseCommand = input("VOCÊ: ")
     
@@ -86,7 +88,24 @@ for i in range(1, 7):
             print("BOT: Vou tentar fazer você rir. Não poderei fazer o mesmo já que não possuo emoções.")
             jokes = random.choice(("O garoto apanhou da vizinha, e a mãe furiosa foi tomar satisfação: Por que a senhora bateu no meu filho? Ele foi mal-educado, e me chamou de gorda. E a senhora acha que vai emagrecer batendo nele?", "Na aula de química o professor pergunta:\n- Quais as principais reações do álcool?\n- Chorar pela ex, achar que esta rico, ficar valente e pegar mulher feia\n- Tirou 10!", "Marido: Estou de saco cheio. Você fica com um lado da casa que eu fico com o outro.\nEsposa: OK. Você fica com o lado de fora.","Você sabe por que todo japonês tem tamanha inteligência?\nPorque, ele tem um micro entre as pernas!","Você sabe o que os testemunhas de Jeová e os testículos tem em comum?\nÉ que os dois sempre andam juntos e ninguém os deixa entrar.","Qual a semelhança entre o ginecologista e um entregador de pizza?\nAmbos podem ver, cheirar, mas não podem comer.","A freira mestre disse: Hoje de sobremesa temos banana!\nEEEEEHHHH!\nEm rodelas\nAAAHHHH!","Mulher é como o circo, o espetáculo está por baixo do pano","Mulher é igual filme: Só se revela no escuro!","Porque a China tem tanta gente?\nÉ por que eles comem com dois pauzinhos.","A filha chega em casa aos prantos: Mãe, fui violentada por um argentino!\nMas, como você sabe que era um argentino?\nEle me fez agradecer","Qual é a melhor marca de aspirador de pó na argentina?\nMaradona ltda."))
             print("BOT:", jokes)
-
+        # Comando Somar
+        if cl == "somar":
+            somar = random.choice(("Soma é comigo mesmo", "Venimim, que é sucesso"))
+            print("BOT:", somar)
+            print("BOT: Quantos números deseja somar?\n2\n3")
+            howManySumAnswer = int(input("VOCÊ: "))
+            if howManySumAnswer == 2:
+                    ChooseTwoSum1 = int(input("BOT: Escolha um número "))
+                    ChooseTwoSum2 = int(input("BOT: Escolha um outro número "))
+                    print("BOT: A soma de", ChooseTwoSum1, "e", ChooseTwoSum2, "é:", ChooseTwoSum1+ChooseTwoSum2)
+                    
+            if howManySumAnswer == 3:
+                    ChooseThreeSum1 = int(input("BOT: Escolha um número "))
+                    ChooseThreeSum2 = int(input("BOT: Escolha um outro número "))
+                    ChooseThreeSum3 = int(input("BOT: Escolha um outro número "))
+                    print("BOT: A soma de", ChooseThreeSum1, ",", ChooseThreeSum2, "e", ChooseThreeSum3, "é:", ChooseThreeSum1+ChooseThreeSum2+ChooseThreeSum3)
+            
+            
             
     break
 
