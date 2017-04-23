619204a4d1402dad1bd73d80c17cdd5732f53a0a
Gabriel Kalani <gabrielsilva1956@gmail.com>
2017-04-23 17:19:55 -0300

    Pequenas mudanças

--- Python/bot.py
+++ Python/bot.py
@@ -7,15 +7,18 @@ import time
 import socket
 import datetime as dt 
 import webbrowser
+import platform
+
+import pygoogle
 
 from datetime import date
-
+from time import strftime
 # ----------------------------
 # Variables
 greeting = random.choice(("Oi", "Olá", "Papai, é você?", "E aí", "kk eae men", "Irineu"))
 howICanHelpYou = random.choice(("Precisa de ajuda em quê?", "Como eu posso te ajudar?"))
 
-commands = ("horario","piada","pesquisa","meuip","data","")
+commands = ("horario","piada","pesquisa","meuip","data","meusistema","localizacao","")
 
 data = date.today()
 hora = dt.datetime.now().hour
@@ -23,9 +26,11 @@ minuto = dt.datetime.now().minute
 
 jokes = random.choice(("O garoto apanhou da vizinha, e a mãe furiosa foi tomar satisfação: Por que a senhora bateu no meu filho? Ele foi mal-educado, e me chamou de gorda. E a senhora acha que vai emagrecer batendo nele?", "Na aula de química o professor pergunta:\n- Quais as principais reações do álcool?\n- Chorar pela ex, achar que esta rico, ficar valente e pegar mulher feia\n- Tirou 10!", "Marido: Estou de saco cheio. Você fica com um lado da casa que eu fico com o outro.\nEsposa: OK. Você fica com o lado de fora.","Você sabe por que todo japonês tem tamanha inteligência?\nPorque, ele tem um micro entre as pernas!","Você sabe o que os testemunhas de Jeová e os testículos tem em comum?\nÉ que os dois sempre andam juntos e ninguém os deixa entrar.","Qual a semelhança entre o ginecologista e um entregador de pizza?\nAmbos podem ver, cheirar, mas não podem comer.","A freira mestre disse: Hoje de sobremesa temos banana!\nEEEEEHHHH!\nEm rodelas\nAAAHHHH!","Mulher é como o circo, o espetáculo está por baixo do pano","Mulher é igual filme: Só se revela no escuro!","Porque a China tem tanta gente?\nÉ por que eles comem com dois pauzinhos.","A filha chega em casa aos prantos: Mãe, fui violentada por um argentino!\nMas, como você sabe que era um argentino?\nEle me fez agradecer","Qual é a melhor marca de aspirador de pó na argentina?\nMaradona ltda."))
 
-tabUrl= "http://www.google.com/?#q="
+urlGoogle= "http://www.google.com/?#q="
 new = 2
 questionSearch = random.choice(("O que deseja pesquisar hoje?","O que deseja saber?","Quer procurar o quê hoje?"))
+
+num_page = 3
 
 ip_address = socket.gethostbyname(socket.gethostname())
 
@@ -50,7 +55,7 @@ for i in range(1, 7):
 
     while 1:
 
-        print("\nBOT: Comandos disponíveis:\nHorário\nPiada\nPesquisa\nMeuIP\n")
+        print("\nBOT: Comandos disponíveis:\nData\nHorário\nPiada\nPesquisa\nMeuIP\nMeuSistema\nLocalização\n")
         chooseCommand = input("VOCÊ: ")
         
         cl = chooseCommand
@@ -61,12 +66,11 @@ for i in range(1, 7):
             
         # Comando Data
         if cl == "data":
-            print("Hoje é:", data)
+            print("BOT: Hoje é:", data)
             
         # Comando Horário
         if cl == "horario":
-            print("Hoje é:", data)
-            print("Pela sua localização atual vejo que são:", hora, ":", minuto)
+            print("BOT: Pela sua localização atual vejo que são:", hora, ":", minuto)
 
         # Comando Piada
         if cl == "piada":
@@ -76,15 +80,32 @@ for i in range(1, 7):
         if cl == "pesquisa":
             print("BOT:", questionSearch)
             search = input("VOCÊ: ")
-            print("No momento estou sem uma função para isso. Não esquenta que em alguns segundos vou mandar sua pesquisa para seu navegador.")
-            time.sleep(random.randint(5, 7))
-            webbrowser.open(tabUrl+search, new=new)
+            if search == "nada":
+                print("BOT: Nada? Isso tá errado ae. Depois dessa vou embora! Tchau!")
+                time.sleep(2)
+                break
+            else:
+                print("BOT: No momento estou sem uma função para isso. Não esquenta, em alguns segundos vou mandar sua pesquisa para seu navegador.")
+                time.sleep(random.randint(3, 5))
+                webbrowser.open(urlGoogle+search, new=new)
             
-        # Comando IP
+        # Comando MeuIP
         if cl == "meuip":
             if ip_address != "127.0.0.1":
                 print("BOT: Seu IP é", ip_address)
             else:
-                print("Você deve estar sem acesso a internet, já que seu IP está como:", ip_address)
-            
+                print("BOT: Você deve estar sem acesso a internet, já que seu IP está como:", ip_address)
+                
+        # Comando MeuSistema
+        if cl == "meusistema":
+            if platform.system() == "Linux":
+                print("BOT: Você está usando", platform.system(), platform.release())
+            elif platform.system() == "Windows" or platform.system() == "win32":
+                print("BOT: Você está usando",platform.system(), platform.release())
+            elif platform.system() == "darwin":
+                print("BOT: Você está usando OS X", platform.system(), platform.release())
+        # Comando MeuSistema
+        if cl == "localizacao":
+            print("")
+        
     break
--- tkinter/bot.py
+++ tkinter/bot.py
@@ -15,16 +15,18 @@ root.geometry("640x400")
 root.resizable(0,0)
 root["background"] = "#14191F"
 
-lbYOU = Label(root, text = 'VOCÊ:')
-lbYOU.place(x=3, y=349)
+lbYOU = Label(root, text = '> ')
+lbYOU.place(x=3, y=348)
 
 bt = Button(root, width=20, text="ENVIAR")
-bt.place(x=483, y=349)
+bt.place(x=483, y=348)
 
 t_frame = Frame(root)
 t_frame.grid(row=4, sticky=W, padx=50 ,pady=348)
 t_text = Text(t_frame, width=49, height=1)
 
 t_text.pack(side=LEFT, fill=BOTH)
+
+
 
 root.mainloop()
