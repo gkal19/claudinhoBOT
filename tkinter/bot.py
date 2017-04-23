644c6a4d75938cbffacc2f44997cc99ea939fa75
Gabriel Kalani <gabrielsilva1956@gmail.com>
2017-04-22 23:23:53 -0300

    A little changes

--- tkinter/bot.py
+++ tkinter/bot.py
@@ -2,17 +2,29 @@
 
 # -----------------------
 # Dependencies
-import tkinter as tk
+from tkinter import *
 
 # -----------------------
 # Variables
-window = tk.Tk()
+root = Tk()
 
 # -----------------------
 # Code
-window.title("BOT")
-window.geometry("640x400")
-window.resizable(0,0)
-window["background"] = "#14191F"
+root.title("BOT")
+root.geometry("640x400")
+root.resizable(0,0)
+root["background"] = "#14191F"
 
-window.mainloop()
+lbYOU = Label(root, text = 'VOCÊ:')
+lbYOU.place(x=3, y=349)
+
+bt = Button(root, width=20, text="ENVIAR")
+bt.place(x=483, y=349)
+
+t_frame = Frame(root)
+t_frame.grid(row=4, sticky=W, padx=50 ,pady=348)
+t_text = Text(t_frame, width=49, height=1)
+
+t_text.pack(side=LEFT, fill=BOTH)
+
+root.mainloop()
