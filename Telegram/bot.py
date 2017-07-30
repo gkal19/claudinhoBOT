84af11e4ed4dd772a1061e7daec776cfb39259a9
Gabriel Kalani <gabrielsilva1956@gmail.com>
2017-07-29 21:48:16 -0300

    Piada e MODO PUTASSO

--- Telegram/bot.py
+++ Telegram/bot.py
@@ -17,26 +17,32 @@ def start(bot, update):
 def mytoken(bot, update):
     update.message.reply_text('Você quer meu Token? Ok né...')
     update.message.reply_text("Peraí...")
-    time.sleep(3)
+    time.sleep(2)
     update.message.reply_text('312719685:5bc4aaaa3385-6542ae862a172c19fb65Cc')
     update.message.reply_text(emojize('TROSLEI KKKKKKKKKKKKKKJJJLOL :joy: :ok_hand:', use_aliases=True))
 
 def restart(bot, update):
 	update.message.reply_text("Reiniciando...")
-	time.sleep(5)
-	update.message.reply_text("MENTIRA! HAHAHAHAHAHAHAH")
+	time.sleep(3)
+	update.message.reply_text("MENTIRA!")
 
+def ping(bot, update):  
+        update.message.reply_text("PING PONG É O CARALHO!")
+	
 def chat(bot, update):
     chat_id = update.message.chat_id
     message = update.message.text
     if 'manda' and 'nudes' in message:
             bot.sendChatAction(chat_id=chat_id, action=telegram.ChatAction.UPLOAD_PHOTO)
             bot.sendPhoto(chat_id=chat_id, photo='http://i.imgur.com/DjMT3QT.png')
+    if ('manda' and 'piada' in message) or ('conta' and 'piada' in message):
+            update.message.reply_text("Não sou bom com piadas...")
 
 updater = Updater('TOKEN')
 updater.dispatcher.add_handler(CommandHandler('start', start))
 updater.dispatcher.add_handler(CommandHandler('mytoken', mytoken))
 updater.dispatcher.add_handler(CommandHandler('restart', restart))
+updater.dispatcher.add_handler(CommandHandler('ping', ping))
 updater.dispatcher.add_handler(MessageHandler([Filters.text], chat))
 updater.start_polling()
 updater.idle()
